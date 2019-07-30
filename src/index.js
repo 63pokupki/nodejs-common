c8901e50927eeea9f210fe9c0f6922fd5a948bfe
angel <Angelrinascita@gmail.com>
2019-07-30 08:45:46 +0400

    1)начал экпериментов

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+export import BaseCtrl from './System/BaseCtrl'
+export import BaseSQL from './System/BaseSQL'
+export import BaseM from './System/BaseM'
+export import ModelValidatorSys from './System/ModelValidatorSys'
+
+export import ModelOneRuleC from './Components/ModelOneRuleC'
+export import ModelRulesC from './Components/ModelRulesC'
