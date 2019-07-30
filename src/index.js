ffcb837e8e9980f96d11c4815af3202b25b01488
angel <Angelrinascita@gmail.com>
2019-07-30 09:39:20 +0400

    1)что то там

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+
 export import BaseCtrl from './System/BaseCtrl'
 export import BaseSQL from './System/BaseSQL'
 export import BaseM from './System/BaseM'
@@ -6,3 +7,15 @@ export import ModelValidatorSys from './System/ModelValidatorSys'
 
 export import ModelOneRuleC from './Components/ModelOneRuleC'
 export import ModelRulesC from './Components/ModelRulesC'
+
+// /* LEGO ошибок */
+export import ErrorSysMiddleware from './System/Middleware/ErrorSysMiddleware'
+
+/* Создает объект запроса */
+export import RequestSysMiddleware from './System/Middleware/RequestSysMiddleware'
+
+/* Создает объект ответа */
+export import ResponseSysMiddleware from './System/Middleware/ResponseSysMiddleware'
+
+// /* проверка авторизации на уровне приложения */
+export import AuthSysMiddleware from './System/Middleware/AuthSysMiddleware'
