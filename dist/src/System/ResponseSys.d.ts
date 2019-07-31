01e7720ddedf7c38579820098bf341747d932b22
angel <Angelrinascita@gmail.com>
2019-07-31 16:28:10 +0400

    еще раз

--- dist/src/System/ResponseSys.d.ts
+++
@@ -1,8 +0,0 @@
-import MainRequest from './MainRequest';
-export declare class ResponseSys {
-    private env;
-    private ifDevMode;
-    private errorSys;
-    constructor(req: MainRequest);
-    response(data: any, sMsg: string): any;
-}
