79edfc5890d8e2111b5cd7ebba84055f14f8faaa
angel <Angelrinascita@gmail.com>
2019-07-31 10:04:01 +0400

    1)добавил еще системных библиотек

--- index.ts
+++ index.ts
@@ -3,6 +3,11 @@ import BaseSQL from './src/System/BaseSQL'
 import BaseM from './src/System/BaseM'
 import { ModelValidatorSys } from './src/System/ModelValidatorSys'
 
+import { ErrorSys } from './src/System/ErrorSys';
+import { UserSys } from './src/System/UserSys'
+import { ResponseSys } from './src/System/ResponseSys'
+
+
 import { ModelOneRuleC } from './src/Components/ModelOneRuleC'
 import { ModelRulesC } from './src/Components/ModelRulesC'
 
@@ -17,6 +22,7 @@ import ResponseSysMiddleware from './src/System/Middleware/ResponseSysMiddleware
 
 // /* проверка авторизации на уровне приложения */
 import AuthSysMiddleware from './src/System/Middleware/AuthSysMiddleware'
+import { RedisSys } from './src/System/RedisSys';
 
 
 export default {
@@ -26,6 +32,10 @@ export default {
     ModelValidatorSys,
     ModelOneRuleC,
     ModelRulesC,
+    ErrorSys,
+    UserSys,
+    ResponseSys,
+    RedisSys,
     Middleware: {
         ErrorSysMiddleware,
         RequestSysMiddleware,
