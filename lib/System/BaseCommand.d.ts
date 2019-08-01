8493dd8f808ae334b518a6a3a4d68b0d16e8d4ac
angel <Angelrinascita@gmail.com>
2019-08-01 14:12:21 +0400

    1)build

--- lib/System/BaseCommand.d.ts
+++ lib/System/BaseCommand.d.ts
@@ -2,8 +2,8 @@ import { ErrorSys } from './ErrorSys';
 import MainRequest from './MainRequest';
 import { UserSys } from './UserSys';
 export default class BaseCommand {
-    protected db: any;
-    protected errorSys: ErrorSys;
-    protected userSys: UserSys;
+    db: any;
+    errorSys: ErrorSys;
+    userSys: UserSys;
     constructor(req: MainRequest);
 }
--- lib/System/BaseTest.d.ts
+++ lib/System/BaseTest.d.ts
@@ -2,8 +2,8 @@ import { ErrorSys } from './ErrorSys';
 import MainRequest from './MainRequest';
 import { UserSys } from './UserSys';
 export default class BaseTest {
-    protected db: any;
-    protected errorSys: ErrorSys;
-    protected userSys: UserSys;
+    db: any;
+    errorSys: ErrorSys;
+    userSys: UserSys;
     constructor(req: MainRequest);
 }
