287f1a6495ab1ff4afbe36ea330d331ef1b999b1
angel <Angelrinascita@gmail.com>
2019-08-29 15:44:13 +0400

    Добавил кеш функцию в SQL

--- src/System/BaseSQL.ts
+++ src/System/BaseSQL.ts
@@ -41,4 +41,46 @@ export default class BaseSQL {
             this.errorSys.error('db_redis', 'Отсутствует подключение к redis');
         }
     }
+
+
+    /**
+     * Авто кеширование для встраивания в функцию
+     * @param sKey - Ключ кеша
+     * @param iTimeSec - Время кеширования
+     * @param callback - функция получающая данные из БД
+     */
+    async autoCache(sKey:string, iTimeSec:number, callback:any):Promise<any>{
+
+        let ok = this.errorSys.isOk();
+
+        let bCache = false; // Наличие кеша
+
+        let sCache = null;
+        let out:any;
+        if( ok ){ // Пробуем получить данные из кеша
+            sCache = await this.redisSys.get(sKey);
+
+            if( sCache ){
+                bCache = true;
+                this.errorSys.devNotice(
+                    sKey, 'Значение взято из кеша'
+                );
+            }
+        }
+
+        if( ok && !bCache ){ // Если значения нет в кеше - добавляем его в кеш
+            this.redisSys.set(
+                sKey,
+                JSON.stringify(await callback()),
+                iTimeSec
+            );
+        }
+
+        if( ok && bCache ){ // Если значение взято из кеша - отдаем его в ответ
+            out = JSON.parse(sCache);
+        }
+
+        return out;
+
+    }
 }
