ead632fdb6aefb97401c1ac3c406e39d7d1ff9d9
Ilyes <garifullinim@63pokupki.ru>
2019-10-08 15:13:16 +0400

    MainConfig moved to separate interface

--- src/System/MainRequest.ts
+++ src/System/MainRequest.ts
@@ -4,7 +4,7 @@ import { ResponseSys } from './ResponseSys';
 
 import { Request } from 'express';
 import { SeoBase } from '../Components/Seo';
-import { SeoConfigI } from '../Components/Seo';
+import { MainConfig } from './MainConfig';
 
 export default interface MainRequest extends Request {
     headers: { [key: string]: any };
@@ -20,65 +20,7 @@ export default interface MainRequest extends Request {
         responseSys: ResponseSys,
         seo?: SeoBase;
     };
-    conf: { // Конфигурация
-        // ================================
-        mysql: { // Конфиг для MySql
-            client: string, // mysql
-            connection: { // Параметры соединения
-                host: string; // "127.0.0.1"
-                user: string; // Пользователь
-                password: string; // Пароль
-                database: string; // Имя базы данных
-            },
-            pool: { min: number, max: number }, // Количество соединений
-            migrations: {
-                tableName: string; // "knex_migrations",
-                directory: string; // "./src/Infrastructure/SQL/Migrations"
-            },
-            acquireConnectionTimeout: number; // таймоут 60000
-        };
-        // ================================
-        pgsql: { // Конфиг для Postgress
-            dialect: string; // "postgres",
-            username: string; // Имя пользователя,
-            password: string; // Пароль
-            host: string; // "127.0.0.1",
-            port: number; // 5432,
-            database: string; // Имя базы данных
-            dialectOptions: {
-                supportBigNumbers: true;
-                decimalNumbers: true;
-            }
-        };
-        // ================================
-        redis: { // Конфиг для редиса
-            url: string; // "redis://127.0.0.1:6379"
-        };
-        // ================================
-        common: { // Общее
-            env: string; // Тип окружения
-            oldCoreURL: string; // URL адрес основного сайта
-            errorMute: boolean;
-            hook_url: string; // Сообщения об ошибках matermost
-            port: number; // порт на котором будет работать экземпляр ноды
-        };
-
-        rabbit: {
-            connection: string;
-            queryList: string[];
-        };
-
-        S3: {
-            endpoint: string;
-            bucket: string;
-            baseUrl: string;
-            access: string;
-            secret: string;
-        },
-
-        SeoConfig?: SeoConfigI
-
-    },
+    conf: MainConfig,
     infrastructure: {
         mysql: any;
         redis: any;
