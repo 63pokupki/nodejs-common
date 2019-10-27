6730c50415709884c49661f364c21153a61647b5
ma3obblu <Ma3oBblu@gmail.com>
2019-10-27 11:45:46 +0400

    add getObject test for S3

--- tests/S3.test.ts
+++ tests/S3.test.ts
@@ -1,10 +1,11 @@
 // process.env.TS_CONFIG_PATHS = 'true';
-var fs = require('fs');
-
-const assert = require('chai').assert;
-
+import { getS3objectParamsI } from "../lib/System/S3DO";
 import { S3DO, S3objectParamsI, setS3objectAclParamsI } from "../src/System/S3DO";
 import { S3confDO } from "./S3DO";
+
+const fs = require('fs');
+
+const assert = require('chai').assert;
 
 /**
  * обертка для чтения файла
@@ -25,13 +26,11 @@ function readFile(file: string) {
  * API для обновления каталогов
  */
 const run = async () => {
-
     await describe('Проверка работы s3', async () => {
         it('upload', async () => {
             let s3 = new S3DO(S3confDO);
             let data: any = await readFile(__dirname + '/S3.test.ts');
             console.log('Upload file ', __dirname + '/S3.test.ts');
-
             let object: S3objectParamsI = {
                 Bucket: S3confDO.bucket,
                 Key: 'media/S3r3.test.ts',
@@ -40,9 +39,21 @@ const run = async () => {
                 Body: data,
                 ACL: 'public-read'
             };
-            console.log(object);
+            console.log('object =>>', object);
             let resp = await s3.upload(object);
-            console.log(resp);
+            console.log('response =>>', resp);
+            assert.ok(true);
+        }).timeout(10000);
+
+        it('get', async () => {
+            let s3 = new S3DO(S3confDO);
+            let object: getS3objectParamsI = {
+                Bucket: S3confDO.bucket,
+                Key: 'media/S3r3.test.ts',
+            };
+            console.log('object =>>', object);
+            let resp = await s3.getObject(object);
+            console.log('response =>>', resp);
             assert.ok(true);
         }).timeout(10000);
 
@@ -50,12 +61,12 @@ const run = async () => {
             let s3 = new S3DO(S3confDO);
             let object: setS3objectAclParamsI = {
                 Bucket: S3confDO.bucket,
-                Key: 'item/x512/17fb6a11e619328b5cdd73b4ce10815640599uhk24e3r5c.jpg',
+                Key: 'media/S3r3.test.ts',
                 ACL: 'public-read'
             };
-            console.log(object);
+            console.log('object =>>', object);
             let resp = await s3.setObjectAcl(object);
-            console.log(resp);
+            console.log('response =>>', resp);
             assert.ok(true);
         }).timeout(10000);
     });
