285d45cec1f4a906eba3cda02b26493f4103ed7a
angelrinascita <angelrinascita@gmail.com>
2019-12-12 15:37:21 +0400

    поправил контроллер чтобы не вызывал краш при отсутствии авторизации

--- src/System/BaseCtrl.ts
+++ src/System/BaseCtrl.ts
@@ -40,13 +40,13 @@ export default class BaseCtrl {
     public async faAction(msg:string, cbAction:Function){
 
         let out = null;
-        if(!this.errorSys.isOk()){
+        if(this.errorSys.isOk()){
+			out = await cbAction();
+		} else {
 			this.resp.status(401);
 			this.errorSys.error('init_ctrl', 'Авторизация или активация провалились')
-			throw Error('Авторизация или активация провалились');
 		}
 
-		out = await cbAction();
 		this.resp.send(
 			this.responseSys.response(out, msg)
 		);
