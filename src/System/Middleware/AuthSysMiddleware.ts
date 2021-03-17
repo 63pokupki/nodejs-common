ab766aa324781f8903d36623aa8d936047cec440
nullilac <x.o.esdes@gmail.com>
2021-03-17 12:37:18 +0400

    В AuthSys добавлена возможность авторизации по cookie

--- src/System/Middleware/AuthSysMiddleware.ts
+++ src/System/Middleware/AuthSysMiddleware.ts
@@ -1,6 +1,7 @@
 import { MainRequest } from '../MainRequest';
 import { UserSys } from '../UserSys';
 import * as jwt from 'jsonwebtoken';
+import type { Response, NextFunction } from 'express';
 
 /**
  * Ответ декодиирования тикена
@@ -9,30 +10,42 @@ import * as jwt from 'jsonwebtoken';
  * Если пользователь постоянно пользуется сайтом у него будет ощущение бесконечного токена
  * Токен скрыто обновляется если он старше 1 недели
  */
-interface JwtDecodeI{
+interface JwtDecodeI {
 	token?: string; // старый статичный apikey
 	iat?: number; // время создания токена
 	exp?: number; // время действия токена
 }
 
 /* проверка аутентификации на уровне приложения */
-export default async function AuthSysMiddleware(request: MainRequest, response: any, next: any): Promise<void> {
-	if (request.headers.apikey && String(request.headers.apikey)) {
-		if (request.headers.apikey.length > 32) {
+export default async function AuthSysMiddleware(
+	request: MainRequest,
+	response: Response,
+	next: NextFunction,
+): Promise<void> {
+	const apikey = request.cookies.apikey || request.headers.apikey;
+
+	if (apikey) {
+		if (apikey.length > 32) {
 			let decoded: JwtDecodeI = null;
 
 			try {
-				decoded = <any>jwt.verify(request.headers.apikey,
-					request.conf.auth.secret, {
-						algorithms: [<any>request.conf.auth.algorithm],
-					});
+				decoded = jwt.verify(apikey, request.conf.auth.secret, {
+					algorithms: [
+						request.conf.auth.algorithm,
+					] as jwt.Algorithm[],
+				}) as JwtDecodeI;
 			} catch (e) {
-				request.sys.errorSys.error('token_expired_error', 'Время жизни токена закончилось');
+				request.sys.errorSys.error(
+					'token_expired_error',
+					'Время жизни токена закончилось',
+				);
 			}
 
 			// Проверяем что прошло меньше месяца
-			if (decoded) { // Проверяем время жизни токена
-				if ((Date.now() / 1000) < decoded.exp) { // TODO это условие скорей всего не нужно поскольку выскакивает ошибка
+			if (decoded) {
+				// Проверяем время жизни токена
+				if (Date.now() / 1000 < decoded.exp) {
+					// TODO это условие скорей всего не нужно поскольку выскакивает ошибка
 					request.sys.apikey = decoded.token;
 				} else {
 					request.sys.apikey = '';
@@ -40,9 +53,10 @@ export default async function AuthSysMiddleware(request: MainRequest, response: 
 			} else {
 				request.sys.apikey = '';
 			}
-		} else { // Временное решение пока идет разработка
+		} else {
+			// Временное решение пока идет разработка
 			// Дает возможность использовать старый токен
-			request.sys.apikey = request.headers.apikey;
+			request.sys.apikey = apikey;
 		}
 	} else {
 		request.sys.apikey = '';
