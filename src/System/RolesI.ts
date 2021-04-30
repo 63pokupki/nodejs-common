386b979ab465f14741384fa9bd2126124f1aeab9
Алексей Кузнецов <kuznecovav@63pokupki.ru>
2021-04-30 16:30:57 +0400

    rename pvz_moderator role

--- src/System/RolesI.ts
+++ src/System/RolesI.ts
@@ -3,7 +3,7 @@ export enum RolesT {
 	administrators = 'administrators',
 	global_moderators = 'global_moderators',
 	pvz_users = 'pvz_users',
-	pvz_moderator = 'pvz_moderators',
+	pvz_moderator = 'pvz_moderator',
 	secret_club = 'secret_club',
 	bp_operator = 'bp_operator',
 }
