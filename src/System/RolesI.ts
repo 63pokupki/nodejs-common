a01a1560f693c81009181cfad385368d33b70444
Алексей Кузнецов <kuznecovav@63pokupki.ru>
2021-04-30 16:49:10 +0400

    add pvz_moderator role, add UserSys.isPvzModerator()

--- src/System/RolesI.ts
+++ src/System/RolesI.ts
@@ -3,6 +3,7 @@ export enum RolesT {
 	administrators = 'administrators',
 	global_moderators = 'global_moderators',
 	pvz_users = 'pvz_users',
+	pvz_moderators = 'pvz_moderators',
 	secret_club = 'secret_club',
 	bp_operator = 'bp_operator',
 }
