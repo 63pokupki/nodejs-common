aed762dc1d3ce8dbc26bcb0ef764b08255ebfbc9
Алексей Кузнецов <kuznecovav@63pokupki.ru>
2021-04-30 13:31:51 +0400

    add secret_club, bp_operator enum values

--- src/System/RolesI.ts
+++ src/System/RolesI.ts
@@ -3,4 +3,6 @@ export enum RolesT {
 	administrators = 'administrators',
 	global_moderators = 'global_moderators',
 	pvz_users = 'pvz_users',
+	secret_club = 'secret_club',
+	bp_operator = 'bp_operator',
 }
