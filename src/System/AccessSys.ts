d0804711951bdf71c581292ce2bef65f94feaa6a
anuritto <anur.usmanov@gmail.com>
2021-12-01 17:12:13 +0400

    refactor: AccessSys access by role/orgrole

--- src/System/AccessSys.ts
+++ src/System/AccessSys.ts
@@ -137,22 +137,40 @@ export class AccessSys {
 
 	/**
 	 * проверка доступа к роуту по роли
+	 * (обратная совместимость)
 	 */
 	public async accessAction(): Promise<void> {
+		await this.accessByRole();
+	}
+	
+	/**
+	 * проверка доступа к роуту по оргроли
+	 * (обратная совместимость)
+	 */
+	public async accessActionOrg(orgId: number): Promise<void> {
+		await this.accessByOrgRole(orgId);
+	}
+
+
+	/**
+	 * проверка доступа к роуту по роли
+	 */
+	public async accessByRole(): Promise<void> {
 		await this.faListRouteForRole();
 
 		const route = this.ctx.req.url;
 
 		if (!this.routesByRole[route]) {
 			throw this.errorSys.throwAccess('У вас нет доступа к данному роуту по роли на сайте');
+		} else {
+			this.errorSys.devNotice('access_by_role', 'Доступ к роуту по глобальной роли');
 		}
 	}
 
 	/**
 	 * проверка доступа к роуту по оргроли
-	 * @param orgId
 	 */
-	public async accessActionOrg(orgId: number): Promise<void> {
+	public async accessByOrgRole(orgId: number): Promise<void> {
 		let res: boolean;
 
 		await this.faListRouteForOrgrole();
@@ -167,6 +185,8 @@ export class AccessSys {
 
 		if (!res) {
 			throw this.errorSys.throwAccess('У вас нет доступа к данному роуту по роли в организации');
+		} {
+			this.errorSys.devNotice('access_by_orgrole', 'Доступ к роуту по роли в организации');
 		}
 	}
 
